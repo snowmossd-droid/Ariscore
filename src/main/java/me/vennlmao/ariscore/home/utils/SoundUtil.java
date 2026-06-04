286bb703fc3fbd88ecca7b903a8fe491445d70a4
snowmossd-droid <snowmossd@gmail.com>
2026-06-05 05:43:09 +0700

    Update SoundUtil.java

--- src/main/java/me/vennlmao/ariscore/home/utils/SoundUtil.java
+++ src/main/java/me/vennlmao/ariscore/home/utils/SoundUtil.java
@@ -1,25 +1,35 @@
-package me.vennlmao.ariscore.home.utils;
+package me.vennlmao.ariscore.team.utils;
 
-import me.vennlmao.ariscore.home.HomeModule;
+import me.vennlmao.ariscore.team.TeamModule;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 
 public class SoundUtil {
 
-    private static HomeModule plugin;
+    private static TeamModule module;
 
-    public static void init(HomeModule pl) {
-        plugin = pl;
-    }
+    public static void init(TeamModule m) { module = m; }
 
     public static void play(Player player, String key) {
-        String soundName = plugin.getConfig().getString("sounds." + key + ".sound", "");
-        float volume = (float) plugin.getConfig().getDouble("sounds." + key + ".volume", 1.0);
-        float pitch = (float) plugin.getConfig().getDouble("sounds." + key + ".pitch", 1.0);
+        String soundName = module.getConfig().getString("sounds." + key + ".sound", "");
+        float volume = (float) module.getConfig().getDouble("sounds." + key + ".volume", 1.0);
+        float pitch = (float) module.getConfig().getDouble("sounds." + key + ".pitch", 1.0);
         if (soundName.isEmpty()) return;
-        try {
-            Sound sound = Sound.valueOf(soundName);
-            player.playSound(player.getLocation(), sound, volume, pitch);
-        } catch (IllegalArgumentException ignored) {}
+        Sound sound = resolveSound(soundName);
+        if (sound == null) return;
+        player.playSound(player, sound, volume, pitch);
     }
-}
+
+    private static Sound resolveSound(String name) {
+        try {
+            return Sound.valueOf(name.toUpperCase());
+        } catch (IllegalArgumentException ignored) {}
+        try {
+            String key = name.toLowerCase().replace(" ", "_");
+            if (!key.contains(":")) key = "minecraft:" + key;
+            org.bukkit.NamespacedKey nk = org.bukkit.NamespacedKey.fromString(key);
+            if (nk != null) return org.bukkit.Registry.SOUNDS.get(nk);
+        } catch (Exception ignored) {}
+        return null;
+    }
+    }
