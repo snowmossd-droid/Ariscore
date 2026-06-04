d7a6e01ea9577317652b5d1fdc52513bba0c25b4
snowmossd-droid <snowmossd@gmail.com>
2026-06-05 05:55:45 +0700

    Update SoundUtil.java

--- src/main/java/me/vennlmao/ariscore/home/utils/SoundUtil.java
+++ src/main/java/me/vennlmao/ariscore/home/utils/SoundUtil.java
@@ -1,19 +1,21 @@
-package me.vennlmao.ariscore.team.utils;
+package me.vennlmao.ariscore.home.utils;
 
-import me.vennlmao.ariscore.team.TeamModule;
+import me.vennlmao.ariscore.home.HomeModule;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 
 public class SoundUtil {
 
-    private static TeamModule module;
+    private static HomeModule plugin;
 
-    public static void init(TeamModule m) { module = m; }
+    public static void init(HomeModule pl) {
+        plugin = pl;
+    }
 
     public static void play(Player player, String key) {
-        String soundName = module.getConfig().getString("sounds." + key + ".sound", "");
-        float volume = (float) module.getConfig().getDouble("sounds." + key + ".volume", 1.0);
-        float pitch = (float) module.getConfig().getDouble("sounds." + key + ".pitch", 1.0);
+        String soundName = plugin.getConfig().getString("sounds." + key + ".sound", "");
+        float volume = (float) plugin.getConfig().getDouble("sounds." + key + ".volume", 1.0);
+        float pitch = (float) plugin.getConfig().getDouble("sounds." + key + ".pitch", 1.0);
         if (soundName.isEmpty()) return;
         Sound sound = resolveSound(soundName);
         if (sound == null) return;
@@ -32,4 +34,4 @@ public class SoundUtil {
         } catch (Exception ignored) {}
         return null;
     }
-    }
+}
