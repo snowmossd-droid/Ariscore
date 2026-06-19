83c24b6120eb151fc389d7b277d5d638775f8751
snowmossd-droid <snowmossd@gmail.com>
2026-06-19 12:55:30 +0700

    Update SoundUtil.java

--- src/main/java/me/vennlmao/ariscore/home/utils/SoundUtil.java
+++ src/main/java/me/vennlmao/ariscore/home/utils/SoundUtil.java
@@ -17,11 +17,15 @@ public class SoundUtil {
         float volume = (float) plugin.getConfig().getDouble("sounds." + key + ".volume", 1.0);
         float pitch = (float) plugin.getConfig().getDouble("sounds." + key + ".pitch", 1.0);
         if (soundName.isEmpty()) return;
-        try {
-            Sound sound = Sound.valueOf(soundName.toUpperCase());
-            player.playSound(player.getLocation(), sound, volume, pitch);
-        } catch (IllegalArgumentException e) {
-            player.playSound(player.getLocation(), soundName, volume, pitch);
-        }
+
+        player.getScheduler().run(plugin.getPlugin(), t -> {
+            if (!player.isOnline()) return;
+            try {
+                Sound sound = Sound.valueOf(soundName.toUpperCase());
+                player.playSound(player.getLocation(), sound, volume, pitch);
+            } catch (IllegalArgumentException e) {
+                player.playSound(player.getLocation(), soundName, volume, pitch);
+            }
+        }, null);
     }
 }
